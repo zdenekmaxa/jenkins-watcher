cb4487778ce5f62bbef4652e778e60853997e1b7
Zdenek Maxa <zdenekmaxa@yahoo.co.uk>
2015-09-04 00:32:08 +0200

    changes in deployment

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,14 @@ fi
 
 appid=$1
 version="v0-4"
+verbosity=warning
+#verbosity=debug
+
 # old way with Google App Engine SDK
 # appcfg.py --oauth2 update -A $appid .
 # new way via Google Cloud SDK
 # appid = jenkins-watcher
 # version is no longer part of the app.yaml but specified
 # explicitly on CLI
-gcloud preview app deploy ./app.yaml --project $appid --version $version --verbosity debug
+gcloud preview app deploy ./app.yaml --project $appid --version $version --verbosity $verbosity
 # localhost run: gcloud preview app run .
